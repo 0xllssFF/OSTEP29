8722b0eaa9b138a2f80e55a6a60b4ab8eed978e8
0xllssFF <49274774+0xllssFF@users.noreply.github.com>
2019-04-06 12:05:06 +0800

    Update BST-tree.cpp

--- BST-tree.cpp
+++ BST-tree.cpp
@@ -1,34 +1,38 @@
+Concurrent BST-tree 
+
 procedure find(n,v)
 	f=n;
-	if v<f.value then dir=left else dir=right;
+	if v<f.value then dir=left else dir=right
 	s=f.dir;
 	if s!=NULL and s.value!=v then 
 		return find(s,v)
 	else
 		lock(f);
-		if s!=f.dir then 
+		if s!=f.dir then 	//检查 f.dir 是否已经被更改，若被更改，则重新查找
 			unlock(f);
-		return find(f,v)
+			return find(f,v)
 		else return (f,dir) 
 		
 		
 procedure search(v)
 	(f,dir)=find(root,v);
 	s=f.dir;
-	if s!=NULL then print "value v is at s"
-	else print "value v is not in the tree";
+	if s!=NULL then 
+		print "value v is at s"
+	else 
+		print "value v is not in the tree"
 	unlock(f);
 	
 
 procedure insert()
 	(f,dir)=find(root,v)
 	if i.dir != NULL then
-    	print"value v is already in the tree"
-    	unlock(f)
-    else
-    	creat(w)
-    	w.left=NULL
-    	w.right=NULL
-    	w.value=v
-    	f.dir=w
-    	unlock(f)
+    		print"value v is already in the tree"
+    		unlock(f)
+    	else
+    		creat(w)
+    		w.left=NULL
+    		w.right=NULL
+    		w.value=v
+ 		f.dir=w
+    		unlock(f)
