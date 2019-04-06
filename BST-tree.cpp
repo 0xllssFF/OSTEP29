83e587ca1f9949ad86aa15c81042371be56ad6ed
0xllssFF <49274774+0xllssFF@users.noreply.github.com>
2019-04-06 11:54:39 +0800

    Create BST-tree.cpp

---
+++ BST-tree.cpp
@@ -0,0 +1,34 @@
+procedure find(n,v)
+	f=n;
+	if v<f.value then dir=left else dir=right;
+	s=f.dir;
+	if s!=NULL and s.value!=v then 
+		return find(s,v)
+	else
+		lock(f);
+		if s!=f.dir then 
+			unlock(f);
+		return find(f,v)
+		else return (f,dir) 
+		
+		
+procedure search(v)
+	(f,dir)=find(root,v);
+	s=f.dir;
+	if s!=NULL then print "value v is at s"
+	else print "value v is not in the tree";
+	unlock(f);
+	
+
+procedure insert()
+	(f,dir)=find(root,v)
+	if i.dir != NULL then
+    	print"value v is already in the tree"
+    	unlock(f)
+    else
+    	creat(w)
+    	w.left=NULL
+    	w.right=NULL
+    	w.value=v
+    	f.dir=w
+    	unlock(f)
